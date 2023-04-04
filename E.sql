986ca2ef05f0dd8b8f08bd77b2c2bdb6e7faf088
Pranav Sinha <pranavsinha922@gmail.com>
2023-04-04 08:40:23 +0530

    Update E.sql

--- E.sql
+++ E.sql
@@ -8,12 +8,22 @@ INSERT INTO EMPLOYEE(EMPLOYEE_NO, fNAME, lNAME, SALARY, JOINING_DATE, DEPARTMENT
 INSERT INTO EMPLOYEE(EMPLOYEE_NO, fNAME, lNAME, SALARY, JOINING_DATE, DEPARTMENT)VALUES(4, 'TOM', 'JOSE', 60000, TO_DATE('08-DEC-2013', 'DD-MON-YYYY'), 'BANKING');
 INSERT INTO EMPLOYEE(EMPLOYEE_NO, fNAME, lNAME, SALARY, JOINING_DATE, DEPARTMENT)VALUES(5, 'PRANAV', 'SINHA', 40000, TO_DATE('08-DEC-2013', 'DD-MON-YYYY'), 'BANKING');
 INSERT INTO EMPLOYEE(EMPLOYEE_NO, fNAME, lNAME, SALARY, JOINING_DATE, DEPARTMENT)VALUES(6, 'THOMAS', 'SHELBY', 60000, TO_DATE('08-DEC-2013', 'DD-MON-YYYY'), 'BANKING');
+INSERT INTO EMPLOYEE(EMPLOYEE_NO, fNAME, lNAME, SALARY, JOINING_DATE, DEPARTMENT)VALUES(7, 'MONICA', 'PANCHAL', 40000, TO_DATE('08-FEB-2013', 'DD-MON-YYYY'), 'MARKETING');
 
 SELECT * FROM EMPLOYEE; 
 
-SELECT * FROM EMPLOYEE ORDER BY FNAME ASC;            -- Arranges the first names into ascending order
-SELECT * FROM EMPLOYEE ORDER BY FNAME DESC;            -- Arranges the first names into descending order
+SELECT * FROM EMPLOYEE ORDER BY FNAME ASC;                       -- Arranges the first names into ascending order
+SELECT * FROM EMPLOYEE ORDER BY FNAME DESC;                      -- Arranges the first names into descending order
 SELECT * FROM EMPLOYEE ORDER BY FNAME ASC, SALARY DESC;          -- Arranges the first names into ASCENDING order AND salary into descending
+SELECT * FROM EMPLOYEE WHERE FNAME='JOHN';                       -- Gets the details of the employee whose fname is John
+SELECT * FROM EMPLOYEE WHERE FNAME IN('JOHN', 'ROY');            -- Gets the details of the employee whose fname is John AND ROY
+SELECT * FROM EMPLOYEE WHERE FNAME LIKE 'J%';                    -- Gets the details of the employee whose name starts with 'J'
+SELECT * FROM EMPLOYEE WHERE FNAME LIKE '%N';                    -- Gets the details of the employee whose name ENDS with 'N'
+SELECT * FROM EMPLOYEE WHERE FNAME LIKE '%___N';                 -- gets the details of the employee whose name ends with 'N' and has 4 letters
+SELECT * FROM EMPLOYEE WHERE FNAME LIKE 'J___%';                 -- gets the details of the employee whose name starts with 'J' and has 4 letters
+SELECT * FROM EMPLOYEE WHERE SALARY>60000;                       -- gets the deatils of the employee whose salary is more than 60000
+SELECT * FROM EMPLOYEE WHERE SALARY<80000;                       -- gets the deatils of the employee whose salary is less than 80000
+SELECT * FROM EMPLOYEE WHERE SALARY BETWEEN 50000 AND 80000;     -- gets the deatils of the employee whose salary is BETWEEN 50K AND 80K
 
 CREATE TABLE INCENTIVES(EMP_REF_ID NUMBER(3), INC_DATE DATE, AMT NUMBER(5), FOREIGN KEY (EMP_REF_ID) REFERENCES EMPLOYEE);
 
